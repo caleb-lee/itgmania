ca6fde23604f88dcf60108b15910ac5925d3c7f7
Caleb Lee <caleb.a.lee@icloud.com>
2025-07-26 21:22:49 -0400

    Attempt to fix Windows issues

--- extern/CMakeProject-lowlatencydancegamesdk.cmake
+++ extern/CMakeProject-lowlatencydancegamesdk.cmake
@@ -1,8 +1,19 @@
 # Build and statically link lowlatencydancegamesdk project
 
-add_subdirectory("lowlatencydancegamesdk")
+set(LOWLATENCYDANCEGAMESDK_SOURCE_DIR "${SM_EXTERN_DIR}/lowlatencydancegamesdk")
 
-set_property(TARGET "lowlatencydancegamesdk" PROPERTY FOLDER "External Libraries")
+include(ExternalProject)
+ExternalProject_Add(
+  lowlatencydancegamesdk
 
-# Make the headers available to ITGMania
-target_include_directories("lowlatencydancegamesdk" PUBLIC "lowlatencydancegamesdk/include")
+  SOURCE_DIR "${LOWLATENCYDANCEGAMESDK_SOURCE_DIR}"
+  INSTALL_COMMAND ""
+)
+
+ExternalProject_Get_Property(lowlatencydancegamesdk SOURCE_DIR BINARY_DIR)
+set(LOWLATENCYDANCEGAMESDK_INCLUDE_DIR "${SOURCE_DIR}/include" CACHE INTERNAL "lowlatencydancegamesdk include")
+if(WIN32)
+  set(LOWLATENCYDANCEGAMESDK_LIBRARY "${BINARY_DIR}/lowlatencydancegamesdk.lib" CACHE INTERNAL "lowlatencydancegamesdk library")
+else()
+  set(LOWLATENCYDANCEGAMESDK_LIBRARY "${BINARY_DIR}/liblowlatencydancegamesdk.a" CACHE INTERNAL "lowlatencydancegamesdk library")
+endif()
