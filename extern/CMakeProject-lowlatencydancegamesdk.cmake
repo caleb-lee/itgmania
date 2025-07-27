627cd32583231bf86d06484c50cbd0ab8ddafcf7
Caleb Lee <caleb.a.lee@icloud.com>
2025-07-26 23:03:09 -0400

    Builds on Mac as an external static library

--- extern/CMakeProject-lowlatencydancegamesdk.cmake
+++ extern/CMakeProject-lowlatencydancegamesdk.cmake
@@ -1,19 +1,49 @@
-# Build and statically link lowlatencydancegamesdk project
+set(INSTALL_DIR ${CMAKE_BINARY_DIR}/lowlatencydancegamesdk-install)
+set(INCLUDE_DIR ${INSTALL_DIR}/include)
+set(LIB_DIR ${INSTALL_DIR}/lib)
 
-set(LOWLATENCYDANCEGAMESDK_SOURCE_DIR "${SM_EXTERN_DIR}/lowlatencydancegamesdk")
+if(WIN32)
+  set(LIB_PATH ${LIB_DIR}/lowlatencydancegamesdk.lib)
+  set(LIBUSB_PATH ${LIB_DIR}/libusb-1.0.lib)
+else()
+  set(LIB_PATH ${LIB_DIR}/liblowlatencydancegamesdk.a)
+  set(LIBUSB_PATH ${LIB_DIR}/libusb-1.0.a)
+endif()
+
+if(APPLE)
+  LIST(APPEND ARCH_FLAGS
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}
+    -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
+  )
+endif()
 
 include(ExternalProject)
 ExternalProject_Add(
-  lowlatencydancegamesdk
+  lowlatencydancegamesdk_project
 
-  SOURCE_DIR "${LOWLATENCYDANCEGAMESDK_SOURCE_DIR}"
-  INSTALL_COMMAND ""
+  SOURCE_DIR ${SM_EXTERN_DIR}/lowlatencydancegamesdk
+  INSTALL_DIR ${INSTALL_DIR}
+  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
+             -DCMAKE_INSTALL_LIBDIR=${LIB_DIR}
+             ${ARCH_FLAGS}
+  BUILD_IN_SOURCE OFF
+  CONFIGURE_HANDLED_BY_BUILD ON
+  BUILD_BYPRODUCTS "${LIB_PATH}"  # Needed for Ninja generator. See BUILD_BYPRODUCTS docs for details.
 )
 
-ExternalProject_Get_Property(lowlatencydancegamesdk SOURCE_DIR BINARY_DIR)
-set(LOWLATENCYDANCEGAMESDK_INCLUDE_DIR "${SOURCE_DIR}/include" CACHE INTERNAL "lowlatencydancegamesdk include")
-if(WIN32)
-  set(LOWLATENCYDANCEGAMESDK_LIBRARY "${BINARY_DIR}/lowlatencydancegamesdk.lib" CACHE INTERNAL "lowlatencydancegamesdk library")
+# This needs to be created immediately, otherwise top-level project generation will fail.
+# It will be populated by the install step of the external project.
+file(MAKE_DIRECTORY ${INCLUDE_DIR})
+
+add_library(lowlatencydancegamesdk STATIC IMPORTED GLOBAL)
+add_dependencies(lowlatencydancegamesdk lowlatencydancegamesdk_project)
+target_include_directories(lowlatencydancegamesdk INTERFACE "${INCLUDE_DIR}")
+set_property(TARGET lowlatencydancegamesdk PROPERTY IMPORTED_LOCATION "${LIB_PATH}")
+if(APPLE)
+  set_property(TARGET lowlatencydancegamesdk PROPERTY INTERFACE_LINK_LIBRARIES 
+    "${LIBUSB_PATH};-framework Security;-framework CoreFoundation;-framework IOKit")
+elseif(WIN32)
+  set_property(TARGET lowlatencydancegamesdk PROPERTY INTERFACE_LINK_LIBRARIES "${LIBUSB_PATH}")
 else()
-  set(LOWLATENCYDANCEGAMESDK_LIBRARY "${BINARY_DIR}/liblowlatencydancegamesdk.a" CACHE INTERNAL "lowlatencydancegamesdk library")
+  set_property(TARGET lowlatencydancegamesdk PROPERTY INTERFACE_LINK_LIBRARIES "${LIBUSB_PATH}")
 endif()
